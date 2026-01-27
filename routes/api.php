c42605a6d030c217f47946b64952e1bcb95034ea
4wellsolutions <4wellsolutions@gmail.com>
2026-01-27 22:13:53 +0500

    Api Authentication with Email, Pass

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth.basic')->group(function () {
     Route::apiResource('blogs', BlogController::class);
     Route::post('/media', [MediaController::class, 'upload']);
     Route::post('/logout', [AuthController::class, 'logout']);
